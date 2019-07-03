e0e7aa2bb210b97d105576bc21677cc0c5de4ba4
JacobLayton <jlayjlay3@gmail.com>
2019-07-02 23:10:41 -0700

    Created navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import hammer from "./img/hammergif.gif";
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {/* <Navbar /> */}
         <img src={hammer} alt="hammer" />
         <p>Jacob's portfolio is currently under construction</p>
       </header>
