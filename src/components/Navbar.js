13e9bc9c1cff42daea516317ded25ef34849bd43
JacobLayton <jlayjlay3@gmail.com>
2019-07-08 19:47:51 -0700

    Added logo to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,32 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+import Logo from "../img/logo1.png";
 
 class Navbar extends Component {
   render() {
-    return <h1>Navbar</h1>;
+    return (
+      <div>
+        <NavBarWrapper>
+          <NavLinkWrapper exact to="/home">
+            <NavLogoWrapper src={Logo} alt="Logo of J L" />
+          </NavLinkWrapper>
+        </NavBarWrapper>
+      </div>
+    );
   }
 }
+
+const NavBarWrapper = styled.div`
+  height: 30px;
+  background: white;
+`;
+
+const NavLinkWrapper = styled.div`
+  margin: 10px;
+`;
+
+const NavLogoWrapper = styled.img`
+  height: 20px;
+`;
 
 export default Navbar;
