8602cd552da11e9ee45c90b25c74df214ba21be5
JacobLayton <jlayjlay3@gmail.com>
2019-07-18 11:41:00 -0700

    Navbar functionality complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,27 +24,31 @@ class Navbar extends Component {
 }
 
 const NavBarWrapper = styled.div`
-  height: 30px;
-  width: 100%;
-  top: 0;
-  left: 0;
+  height: 5vm;
+  width: 100vw;
   background: white;
+  border: 1px solid red;
+  display: flex;
+  justify-content: space-between;
 `;
 
 const NavLinkWrapper = styled(NavLink)`
   font-size: 1.2rem;
   color: #757582;
   margin: 0 0.5rem;
+  border: 1px solid green;
 `;
 
 const NavLogoWrapper = styled.img`
   height: 40px;
+  border: 1px solid blue;
 `;
 
 const NavLinks = styled.div`
   display: flex;
   justify-content: space-evenly;
   align-items: center;
+  border: 1px solid purple;
 `;
 
 export default withRouter(Navbar);
