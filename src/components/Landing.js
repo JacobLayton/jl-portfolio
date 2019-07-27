df4bd87ab91702f048422416971c8ac96720b19b
JacobLayton <jlayjlay3@gmail.com>
2019-07-26 18:11:14 -0700

    Navbar complete minus styling

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,8 +6,14 @@ import "../styles/landing.css";
 class Home extends Component {
   render() {
     return (
-      <div>
-        <Jumbotron />
+      <div class="landing-container">
+        <div class="parallax" />
+        <p>Hello,my name is Jacob.</p>
+        <p>I'm a Full Stack Web Developer</p>
+        <div class="box1">Hello1</div>
+        <div class="box2">Hello2</div>
+        <div class="box3">Hello3</div>
+        <div class="box4">Hello4</div>
       </div>
     );
   }
@@ -15,16 +21,16 @@ class Home extends Component {
 
 export default Home;
 
-const Jumbotron = styled.div`
-  border: 1px solid yellow;
-  background-image: url(${jumbo2});
-  background-size: cover;
-  background-position: center center;
-  background-repeat: no-repeat;
-  // background-attachment: fixed;
-  max-width: 100%;
-  max-height: 100%;
-`;
+// const Jumbotron = styled.div`
+//   border: 1px solid yellow;
+//   background-image: url(${jumbo2});
+//   background-size: cover;
+//   background-position: center center;
+//   background-repeat: no-repeat;
+//   // background-attachment: fixed;
+//   max-width: 100%;
+//   max-height: 100%;
+// `;
 
 // const Jumbotron = styled.img`
 //   z-index: 0;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,9 +6,21 @@ import Logo from "../img/logo1.png";
 
 class Navbar extends Component {
   render() {
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {
+      console.log(prevScrollpos);
+      var currentScrollPos = window.pageYOffset;
+      if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+      } else {
+        document.getElementById("navbar").style.top = "-50px";
+      }
+      prevScrollpos = currentScrollPos;
+    };
+
     return (
       <div>
-        <NavBarWrapper>
+        <NavBarWrapper id="navbar">
           <NavLinkWrapper exact to="/home">
             <NavLogoWrapper src={Logo} alt="Logo of J L" />
           </NavLinkWrapper>
@@ -24,13 +36,16 @@ class Navbar extends Component {
 }
 
 const NavBarWrapper = styled.div`
-  height: 5vm;
-  max-width: 100vw;
+  height: 5vh;
+  width: 100%;
   background: transparent;
   border: 1px solid red;
   display: flex;
   justify-content: space-between;
   z-index: 999;
+  position: fixed;
+  top: 0;
+  transition: top 0.3s;
 `;
 
 const NavLinkWrapper = styled(NavLink)`
