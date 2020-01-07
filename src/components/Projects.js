7732f6ddcdb1692a2fa2086cd4bc63c4b54b3810
JacobLayton <jlayjlay3@gmail.com>
2020-01-07 15:19:58 -0800

    Added buttons to Projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Navbar2 from "./Navbar2";
-import "../styles/projects.css"
+import "../styles/projects.css";
 import proj6 from "../img/proj6.jpg";
 import proj5 from "../img/proj5.jpg";
 import proj4 from "../img/proj4.jpg";
@@ -13,7 +13,10 @@ class Projects extends Component {
         <div class="projects-main">
           <section class="title-description">
             <h2>Projects</h2>
-            <p>This will be a description of my projects I guess? Defintiely want a short paragraph about what I like to work on and blah blah blah</p>
+            <p>
+              This will be a description of my projects I guess? Defintiely want
+              a short paragraph about what I like to work on and blah blah blah
+            </p>
           </section>
           <section class="project-images">
             <div class="single-project">
@@ -24,10 +27,12 @@ class Projects extends Component {
             <div class="single-project">
               <h5>Currciulum Bot</h5>
               <img src={proj5} />
+              <button>Launch</button>
             </div>
             <div class="single-project">
               <h5>Farm Fresh Produce</h5>
               <img src={proj4} />
+              <button>Launch</button>
             </div>
           </section>
         </div>
