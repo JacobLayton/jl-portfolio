d5c21ccb78bbb308a436278017a8abb546ec1942
JacobLayton <jlayjlay3@gmail.com>
2020-01-14 16:13:17 -0800

    Added contact email logo to footer, linked to contact page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,21 @@
 import React, { Component } from "react";
 import "../styles/footer.css";
+import { Link } from "react-router-dom";
 
 class Footer extends Component {
   render() {
     return (
       <section className="footer">
         <div>
-          <a href="https://github.com/JacobLayton" target="_blank">
-            <i class="fab fa-github"></i>
-          </a>
           <a href="https://www.linkedin.com/in/jacob-layton/" target="_blank">
             <i class="fab fa-linkedin-in"></i>
           </a>
+          <a href="https://github.com/JacobLayton" target="_blank">
+            <i class="fab fa-github"></i>
+          </a>
+          <Link to="/contact">
+            <i class="fas fa-envelope"></i>
+          </Link>
         </div>
         <span>Copyright © Jacob Layton 2019. All Rights Reserved.</span>
       </section>
