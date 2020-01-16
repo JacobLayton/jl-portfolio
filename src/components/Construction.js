bc76c118dcc257225577d875a008574a60e08eec
JacobLayton <jlayjlay3@gmail.com>
2020-01-16 10:55:18 -0800

    Updated construction styles to not conflict with app

--- src/components/Construction.js
+++ src/components/Construction.js
@@ -5,7 +5,7 @@ import "../styles/construction.css";
 class Construction extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="construction">
         <header className="App-header">
           <img src={hammer} alt="hammer" />
           <p>Jacob's portfolio is currently under construction</p>
