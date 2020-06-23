670a4bf498457d74c27ddd28e083d0d9066ce46f
JacobLayton <jlayjlay3@gmail.com>
2020-06-23 15:00:53 -0700

    Updating contect styling

--- src/components/Contact2.js
+++ src/components/Contact2.js
@@ -18,6 +18,7 @@ export default class Contact extends React.Component {
     email: "",
     subject: "",
     text: "",
+    hasSubmittedForm: false,
   };
 
   handleChange = (e) => {
@@ -39,62 +40,76 @@ export default class Contact extends React.Component {
     };
 
     axios
-      .post(`https://portfolio-node-backend.herokuapp.com/email`, { user })
+      // .post(`https://portfolio-node-backend.herokuapp.com/email`, { user })
+      .post(`https://portfolio-node-backend.herokuapp.com`, { user })
       .then((res) => {
         console.log(res);
       })
       .catch((err) => {
         console.log(err);
       });
+    e.target.reset();
+    this.setState({ hasSubmittedForm: true });
   };
+
+  thankYouButton = () => {
+    if (this.hasSubmittedForm) {
+      return <h1>Thank You!</h1>;
+    }
+    return <button type="submit">Submit</button>;
+  };
+
   render() {
     return (
       <div>
         <Navbar2 />
-        <form onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            name="name"
-            placeholder="Name"
-            onChange={this.handleChange}
-            className="input"
-          />
+        <div className="form-card">
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              onChange={this.handleChange}
+              className="input"
+            />
 
-          <input
-            type="email"
-            name="email"
-            placeholder="Email"
-            onChange={this.handleChange}
-            className="input"
-          />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              onChange={this.handleChange}
+              className="input"
+            />
 
-          <input
-            type="text"
-            name="subject"
-            placeholder="Subject"
-            onChange={this.handleChange}
-            className="input"
-          />
+            <input
+              type="text"
+              name="subject"
+              placeholder="Subject"
+              onChange={this.handleChange}
+              className="input"
+            />
 
-          {/* <input
-            type="textarea"
-            name="text"
-            placeholder="Enter message here"
-            onChange={this.handleChange}
-            className="text-input"
-            wrap="pyhsical"
-            rows="10"
-          /> */}
-          <textarea
-            name="text"
-            placeholder="Enter your message here"
-            onChange={this.handleChange}
-            className="text-input"
-            rows="5"
-          ></textarea>
+            {/* <input
+              type="textarea"
+              name="text"
+              placeholder="Enter message here"
+              onChange={this.handleChange}
+              className="text-input"
+              wrap="pyhsical"
+              rows="10"
+            /> */}
+            <textarea
+              name="text"
+              placeholder="Enter your message here"
+              onChange={this.handleChange}
+              className="text-input"
+              rows="5"
+            ></textarea>
+            {/* <this.thankYouButton hasSubmittedForm={false} /> */}
 
-          <button type="submit">Submit</button>
-        </form>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
       </div>
     );
   }
