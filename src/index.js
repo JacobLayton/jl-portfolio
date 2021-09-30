5e703973eba1d8f347f592e282ac1c400ea8bab3
Jacob Layton <jlayjlay3@gmail.com>
2021-09-30 15:35:19 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import { BrowserRouter as Router } from "react-router-dom";
-// import "./index.css";
-// import App from "./App";
-// import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-// ReactDOM.render(
-//   <Router>
-//     <App />
-//   </Router>,
-//   document.getElementById("root")
-// );
+ReactDOM.render(
+  <h1>App is currently under construction</h1>,
+  document.getElementById("root")
+);
